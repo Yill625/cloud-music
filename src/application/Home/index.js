616f7d8567584fec14136eedbd3c50f851302199
易亮亮 <v-yill@vanke.com>
2020-06-02 18:19:12 +0800

    添加路由

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
+import { Top } from './style';
 
 function Home(props) {
   const { route } = props;
 
   return (
     <div>
-      <div>Home</div>
+      <Top>
+        <span className="iconfont menu">&#xe780;</span>
+        <span className="title">WebApp</span>
+        <span className="iconfont search">&#xe780;</span>
+      </Top>
       {renderRoutes(route.routes)}
     </div>
   );
