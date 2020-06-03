1c4a3f9c91a40aca6c4475fcf3a7ef130c01531b
易亮亮 <v-yill@vanke.com>
2020-06-03 11:32:33 +0800

    添加store

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
-import { Top } from './style';
+import { Top, Tab, TabItem } from './style';
+import { NavLink } from 'react-router-dom'; // 利用 NavLink 组件进行路由跳转
 
 function Home(props) {
   const { route } = props;
@@ -8,10 +9,27 @@ function Home(props) {
   return (
     <div>
       <Top>
-        <span className="iconfont menu">&#xe780;</span>
-        <span className="title">WebApp</span>
-        <span className="iconfont search">&#xe780;</span>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">Web App</span>
+        <span className="iconfont search">&#xe62b;</span>
       </Top>
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected">
+          <TabItem>
+            <span> 推荐 </span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/singers" activeClassName="selected">
+          <TabItem>
+            <span> 歌手 </span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/rank" activeClassName="selected">
+          <TabItem>
+            <span> 排行榜 </span>
+          </TabItem>
+        </NavLink>
+      </Tab>
       {renderRoutes(route.routes)}
     </div>
   );
